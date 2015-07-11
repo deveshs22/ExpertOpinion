8a5fd293fa96462047957ecdf63aace8c0b5ab17
deveshs22 <dev.sinha22@gmail.com>
2015-07-11 12:42:32 +0530

    Projects refactoring

--- DataService/Global.asax.cs
+++ DataService/Global.asax.cs
@@ -13,13 +13,9 @@ namespace DataService
     {
         protected void Application_Start()
         {
-            AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
-            //GlobalConfiguration.Configuration.MessageHandlers.Add(new CorsHandler());
-            //GlobalConfiguration.Configuration.MessageHandlers.Add(new DataService.Handlers.AuthenticationHandler1());
         }
     }
 }
